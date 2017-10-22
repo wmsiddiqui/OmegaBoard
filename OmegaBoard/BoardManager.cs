690afcea6c795e07f3b87d60420b5ac3a83655a2
Waqar <wmsiddiqui.6@gmail.com>
2017-10-22 12:14:02 -0500

    Refactored add lane code into different class. Still lacks seperation of concerns.

---
+++ OmegaBoard/BoardManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OmegaBoard
+{
+    public class BoardManager
+    {
+        public void AddLane(FlowLayoutPanel homeBoard)
+        {
+            //first get total lanes
+            var existingLanes = homeBoard.Controls;
+            var existingNumberofLanes = existingLanes.Count;
+            //add new lane
+            var newLane = new GroupBox();
+            newLane.Name = "Lane" + (existingNumberofLanes + 1);
+            newLane.Text = "test" + (existingNumberofLanes + 1);
+            newLane.Width = 200;
+            newLane.Height = homeBoard.Height - 30;
+            var newFlow = new FlowLayoutPanel();
+            newFlow.Name = "Flow" + (existingNumberofLanes + 1);
+
+            newFlow.BackColor = System.Drawing.Color.Aqua;
+            newFlow.Dock = DockStyle.Fill;
+            newFlow.DragEnter += (s, e) =>
+            {
+                e.Effect = DragDropEffects.Move;
+            };
+            newFlow.AllowDrop = true;
+            newFlow.DragDrop += (s, e) =>
+            {
+                Control userControl = GetControl(e);
+                newFlow.Controls.Add(userControl);
+            };
+            //create temporary button to drag
+            var newButton = new Button();
+            newButton.Width = newFlow.Width - 10;
+            newButton.MouseDown += (s, e) =>
+            {
+                newButton.DoDragDrop(new ControlWrapper(newButton), DragDropEffects.Move);
+            };
+
+            //add controls to board
+            newFlow.Controls.Add(newButton);
+            newLane.Controls.Add(newFlow);
+            homeBoard.Controls.Add(newLane);
+        }
+
+        private Control GetControl(DragEventArgs e)
+        {
+            ControlWrapper controlWrapper = e.Data.GetData(typeof(ControlWrapper)) as ControlWrapper;
+            Control userControl = controlWrapper.Control as Control;
+            return userControl;
+        }
+    }
+}
--- OmegaBoard/Form1.cs
+++ OmegaBoard/Form1.cs
@@ -12,57 +12,19 @@ namespace OmegaBoard
 {
     public partial class Form1 : Form
     {
+        private BoardManager boardManager;
+
         public Form1()
         {
             InitializeComponent();
             HomeBoard.WrapContents = false;
             HomeBoard.AutoScroll = true;
+            boardManager = new BoardManager();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //first get total lanes
-            var existingLanes = HomeBoard.Controls;
-            var existingNumberofLanes = existingLanes.Count;
-            //add new lane
-            var newLane = new GroupBox();
-            newLane.Name = "Lane" + (existingNumberofLanes + 1);
-            newLane.Text = "test" + (existingNumberofLanes + 1);
-            newLane.Width = 200;
-            newLane.Height = HomeBoard.Height- 30;
-            var newFlow = new FlowLayoutPanel();
-            newFlow.Name =  "Flow" + (existingNumberofLanes + 1);
-            
-            newFlow.BackColor = Color.Aqua;
-            newFlow.Dock = DockStyle.Fill;
-            newFlow.DragEnter += (s, ee) =>
-            {
-                ee.Effect = DragDropEffects.Move;
-            };
-            newFlow.AllowDrop = true;
-            newFlow.DragDrop += (s, ee) =>
-            {
-                Control userControl = GetControl(ee);
-                newFlow.Controls.Add(userControl);
-            };
-            //create temporary button to drag
-            var newButton = new Button();
-            newButton.Width = newFlow.Width-10;
-            newButton.MouseDown += (s, ee) =>
-            {
-                newButton.DoDragDrop(new ControlWrapper(newButton), DragDropEffects.Move);
-            };
-
-            //add controls to board
-            newFlow.Controls.Add(newButton);
-            newLane.Controls.Add(newFlow);
-            HomeBoard.Controls.Add(newLane);
-        }
-        private static Control GetControl(DragEventArgs e)
-        {
-            ControlWrapper controlWrapper = e.Data.GetData(typeof(ControlWrapper)) as ControlWrapper;
-            Control userControl = controlWrapper.Control as Control;
-            return userControl;
+            boardManager.AddLane(HomeBoard);
         }
     }
     class ControlWrapper
