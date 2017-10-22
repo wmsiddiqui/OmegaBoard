555f93c3ee4d53f7857e04de61f296b008859efb
Waqar <wmsiddiqui.6@gmail.com>
2017-10-22 14:21:16 -0500

    Model for the lanes. Included moving and removing a card and adding card at an index.

---
+++ OmegaBoard/Models/Lane.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmegaBoard.Models
+{
+    public class Lane
+    {
+        private List<Card> _cards;
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        
+        public List<Card> Cards
+        {
+            get { return _cards; }
+        }
+
+        public Card AddCard(string cardText)
+        {
+            var card = new Card(cardText);
+            _cards.Add(card);
+            return card;
+        }
+
+        public void AddCard(Card card, int? index = null)
+        {
+            if (index == null)
+            {
+                _cards.Add(card);
+            }
+            else
+            {
+                _cards.Insert(index.Value, card);
+            }
+        }
+
+        public void MoveCard(Card card, int index)
+        {            
+            if(!_cards.Remove(card))
+            {
+                throw new Exception("unable to move card within lane");
+            }
+            _cards.Insert(index, card);
+        }
+
+        public void RemoveCard(Card card)
+        {
+            if(!_cards.Remove(card))
+            {
+                throw new Exception("Unable to remove card from the lane");
+            }
+        }
+    }
+}
